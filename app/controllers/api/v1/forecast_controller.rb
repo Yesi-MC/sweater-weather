f22c9fe53681842c50ed346a31da84fcfbc71f9f
Yesi Meza <yesi.meza10@gmail.com>
2021-03-06 22:49:25 -0700

    created forecast controller

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::ForecastController < ApplicationController
+  def index
+    conn = Faraday.new('http://www.mapquestapi.com') do |f|
+      f.params['key'] = ENV['map_api_key']
+    end
+    response = conn.get("/geocoding/v1/address") do |r|
+      r.params['location'] = params[:location]
+    end
+    location = JSON.parse(response.body, symbolize_names: true) 
+
+    latitude = location[:results][0][:locations][0][:latLng][:lat]
+    longitude = location[:results][0][:locations][0][:latLng][:lng]
+
+    conn2 = Faraday.new('https://api.openweathermap.org') do |f|
+      f.params['appid'] = ENV['weather_api_key']
+    end
+    response2 = conn2.get("/data/2.5/onecall") do |r|
+      r.params['lat'] = latitude
+      r.params['lon'] = longitude
+      r.params['exclude'] = "minutely,alerts"
+      r.params['units'] = "imperial"
+    end
+    weather = JSON.parse(response2.body, symbolize_names: true) 
+
+    current_weather = CurrentWeather.new(weather[:current])
+    daily_weather = weather[:daily][0..4].map { |daily| DailyWeather.new(daily) }
+    hourly_weather = weather[:hourly][0..7].map { |hourly| HourlyWeather.new(hourly) }
+    total_weather = TotalWeather.new(current_weather, daily_weather, hourly_weather) 
+    render json: ForecastSerializer.new(total_weather)
+  end
+end 
