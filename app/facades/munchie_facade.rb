38cbc6d4402e2e134f9a231729a684b3b47c8bb7
Yesi Meza <yesi.meza10@gmail.com>
2021-03-08 11:10:44 -0700

    added the locationservice call and removed comment

--- app/facades/munchie_facade.rb
+++ app/facades/munchie_facade.rb
@@ -2,25 +2,17 @@ class MunchieFacade
   class << self 
 
     def get_munchies(destination, start_location, food)
-      conn = Faraday.new('http://www.mapquestapi.com') do |f|
-      f.params['key'] = ENV['map_api_key']
+      distance = LocationService.get_distance(start_location, destination)
+
+      longitude = distance[:route][:boundingBox][:lr][:lng]
+      latitude = distance[:route][:boundingBox][:lr][:lat]
+
+      weather = ForecastService.current_forecast(latitude, longitude)
+
+      food_data = FoodService.get_food_data(food, destination)
+
+      Munchie.new(distance, weather, food_data, destination)
     end
-    response = conn.get("/directions/v2/route") do |r|
-      r.params['from'] = start_location
-      r.params['to'] = destination
-    end
-    distance = JSON.parse(response.body, symbolize_names: true)
-    travel_time = distance[:route][:formatted_time]
 
-    longitude = distance[:route][:boundingBox][:lr][:lng]
-    latitude = distance[:route][:boundingBox][:lr][:lat]
-
-    weather = ForecastService.current_forecast(latitude, longitude)
-
-    food_data = FoodService.get_food_data(food, destination)
-    
-    munchie = Munchie.new(distance, weather, food_data, destination)
-
-    end
   end
 end
