5ddf135156b349bab0008db2b1269b80f28e1abb
Yesi Meza <yesi.meza10@gmail.com>
2021-03-08 11:10:59 -0700

    cleaned up the controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,52 +1,10 @@
 class Api::V1::MunchiesController < ApplicationController
   
   def index
-
     destination = params[:destination]
-    conn = Faraday.new('http://www.mapquestapi.com') do |f|
-      f.params['key'] = ENV['map_api_key']
-    end
-    response = conn.get("/directions/v2/route") do |r|
-      r.params['from'] = params[:start]
-      r.params['to'] = params[:destination]
-    end
-    distance = JSON.parse(response.body, symbolize_names: true)
-    travel_time = distance[:route][:formatted_time]
-
-    long = distance[:route][:boundingBox][:lr][:lng]
-    lat = distance[:route][:boundingBox][:lr][:lat]
-
-    conn_2 = Faraday.new('https://api.openweathermap.org') do |f|
-      f.params['appid'] = ENV['weather_api_key']
-    end
-
-    response_2 = conn_2.get("/data/2.5/onecall") do |r|
-      r.params['lat'] = lat
-      r.params['lon'] = long
-      r.params['exclude'] = "minutely,alerts,hourly,daily"
-      r.params['units'] = "imperial"
-    end
-
-    weather = JSON.parse(response_2.body, symbolize_names: true)
-
-    summary = weather[:current][:weather][0][:description] 
-    temp = weather[:current][:temp] 
-
-    conn_3 = Faraday.new('https://api.yelp.com') do |f|
-      f.headers['Authorization'] = ENV['yelp_api_key']
-    end
-
-    response_3 = conn_3.get("/v3/businesses/search") do |r|
-      r.params['term'] = params[:food]
-      r.params['location'] = params[:destination]
-    end
-
-    food = JSON.parse(response_3.body, symbolize_names: true) 
-    name = food[:businesses][0][:name]
-    address = food[:businesses][0][:location][:display_address]
-
-    munchie = Munchie.new(distance, weather, food, destination)
-
+    start_location = params[:start]
+    food = params[:food]
+    munchie = MunchieFacade.get_munchies(destination, start_location, food)
     render json: MunchieSerializer.new(munchie)
   end 
 end 
