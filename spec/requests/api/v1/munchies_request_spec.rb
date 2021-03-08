0392962c4ab4f9de5a5c46424c4e1ec03b74fb64
Yesi Meza <yesi.meza10@gmail.com>
2021-03-08 09:39:42 -0700

    updated the rest of the munchies spec

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -11,6 +11,50 @@ describe "Yelp API Endpoints" do
       expect(response).to be_successful 
       expect(response.status).to eq(200)
 
+      munchies = JSON.parse(response.body, symbolize_names: true)
+
+      expect(munchies).to be_a(Hash)
+      expect(munchies).to have_key(:data)
+      expect(munchies[:data]).to be_a(Hash)
+
+      expect(munchies[:data]).to have_key(:id)
+      expect(munchies[:data][:id]).to eq(nil)
+
+      expect(munchies[:data]).to have_key(:type)
+      expect(munchies[:data][:type]).to eq('munchie')
+
+      expect(munchies[:data]).to have_key(:attributes)
+      expect(munchies[:data][:attributes]).to be_a(Hash)
+
+      munchie_data = munchies[:data][:attributes]
+
+      expect(munchie_data).to have_key(:destination_city)
+      expect(munchie_data[:destination_city]).to be_a(String)
+
+      expect(munchie_data).to have_key(:travel_time)
+      expect(munchie_data[:travel_time]).to be_a(String)
+
+      expect(munchie_data).to have_key(:forecast)
+      expect(munchie_data[:forecast]).to be_a(Hash)
+
+      expect(munchie_data).to have_key(:restaurant)
+      expect(munchie_data[:restaurant]).to be_a(Hash)
+
+      forecast_data = munchies[:data][:attributes][:forecast]
+
+      expect(forecast_data).to have_key(:summary)
+      expect(forecast_data[:summary]).to be_a(String)
+
+      expect(forecast_data).to have_key(:temperature)
+      expect(forecast_data[:temperature]).to be_a(String)
+
+      rest_data = munchies[:data][:attributes][:restaurant]
+
+      expect(rest_data).to have_key(:name)
+      expect(rest_data[:name]).to be_a(String)
+
+      expect(rest_data).to have_key(:address)
+      expect(rest_data[:address]).to be_a(String)
     end
   end
 end
