fd2d361cb31c16ff00fb74bcb2caa77512860a3f
Yesi Meza <yesi.meza10@gmail.com>
2021-03-09 12:56:34 -0700

    refactored into service and facade

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,15 +1,8 @@
 class Api::V1::BackgroundController < ApplicationController
 
   def index 
-    conn = Faraday.new('https://api.unsplash.com') do |f|
-      f.params['client_id'] = ENV['unsplash_api_key']
-    end
-    response = conn.get("/search/photos") do |r|
-      r.params['query'] = params[:location]
-      r.params['per_page'] = 1
-    end
-    image = JSON.parse(response.body, symbolize_names: true) 
-  
-    # require 'pry'; binding.pry
+    location = params[:location]
+    background = ImageFacade.obtain_image(location)
+    render json: ImageSerializer.new(background)
   end
 end 
